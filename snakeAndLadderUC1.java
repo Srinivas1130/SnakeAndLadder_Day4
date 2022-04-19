dcad3c8ee5568f45a31941155cb6b248fb6bc715
Srinivas1130 <srinivasachar1998@gmail.com>
2022-04-19 10:00:01 +0530

    Added-Welcome to snake and ladder code

---
+++ snakeAndLadderUC1.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day4_snakeandladder;
+public class snakeAndLadderUC1 
+{
+	static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+    	
+        System.out.println("Welcome to snake ladder game");
+        System.out.println("Player will start from zero position.");
+    }
+
+}
